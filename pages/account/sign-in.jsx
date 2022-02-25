b8b7aa218e1d07d0af81ca7277d5acae45d3d23b
Phan Duc Anh <ducanhit1997@gmail.com>
2022-02-25 17:56:55 +0700

    test commit

--- pages/account/sign-in.jsx
+++ pages/account/sign-in.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { PageLayout } from "../../src/components/layout/page-layout";
 import { useRouter } from "next/router";
 const MainPage = () => {
-  return <PageLayout>Page Sign in</PageLayout>;
+  return <PageLayout>Page Sign in 1</PageLayout>;
 };
 
 export default MainPage;
